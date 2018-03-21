79c8aaaf0963382d9b45a4e870566fd3eebd9089
jsh2174 <37024397+jsh2174@users.noreply.github.com>
2018-03-22 01:09:04 +0900

    Update pushPrint.java

--- HelloGit/src/pushPrint.java
+++ HelloGit/src/pushPrint.java
@@ -6,6 +6,12 @@ public class pushPrint
 
 	private String msg = "local update ";
 
-	public void show () { System.out.println(s);}
+	public void show1 () { System.out.println(s);}
+	public void show2 () { System.out.println(s);}
+
+	public void show3 () { System.out.println(s);}
+
+	public void show4 () { System.out.println(s);}
+
 
 }
