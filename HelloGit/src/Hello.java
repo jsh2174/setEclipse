4873aa62926ee82eb3a5c2a5875a6d79d6f91dc7
jsh <jsh@LAPTOP-GSIC3COA>
2018-03-22 01:31:30 +0900

    ... 이런....

--- HelloGit/src/Hello.java
+++ HelloGit/src/Hello.java
@@ -2,14 +2,8 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello git1234");
-		System.out.println("hello git1");
-		System.out.println("add hotfix------------------");
-		//hotfix 전환 후...
-		System.out.println("hotfix 전환 후...");
-		// merge 후 추가...
-		System.out.println("merge after !!");
+	
+		String s = "다른 이클립스에서 수정함...";
 		
 		
 	}
